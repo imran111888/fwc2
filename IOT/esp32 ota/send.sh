614abcf0a4797e43e41967653afec48e9586e391
imran <113091364+imran111888@users.noreply.github.com>
2022-11-29 13:57:01 +0530

    Add files via upload

---
+++ IOT/esp32 ota/send.sh
@@ -0,0 +1,5 @@
+cd .pio/build/esp32doit-devkit-v1
+scp firmware.bin imran@192.168.43.48:/home/imran/esp32_lap/esp32Lap/.pio/build/esp32doit-devkit-v1
+
+
+#scp firmware.bin manideep@192.168.53.233:/home/manideep/FWC/esp32/.pio/build/esp32doit-devkit-v1/
---
+++ IOT/esp32 ota/vaman.sh
@@ -0,0 +1,1 @@
+pio run -t nobuild --upload-port 192.168.43.102
